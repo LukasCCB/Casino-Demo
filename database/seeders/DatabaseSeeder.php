477ba75004e7932b5ade2abd794fa361d062c0e6
LukasCCB <lukasccbb@gmail.com>
2024-03-17 16:23:27 -0300

    Added setting seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(CreateUserSeeder::class);
+        $this->call(SettingSeeder::class);
 
         // User::factory(10)->create();
 
